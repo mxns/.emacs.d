498cd02824a031e5588fb02555461dd5bbefab51
Måns Rolandi <mxns@users.noreply.github.com>
2025-03-07 16:00:37 +0100

    Friendly scrolling

--- friendly.el
+++ friendly.el
@@ -1,8 +1,25 @@
-;;;; Mouse scrolling in terminal emacs
-(unless (display-graphic-p)
-  ;; activate mouse-based scrolling
-  (xterm-mouse-mode 1)
-  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
-  (global-set-key (kbd "<mouse-6>") (lambda () (interactive) (scroll-left -1)))
-  (global-set-key (kbd "<mouse-7>") (lambda () (interactive) (scroll-left 1))))
+
+(setq mxns/mouse 1)
+
+(defun mxns/toggle-mouse ()
+  (interactive)
+  (unless (display-graphic-p)
+    (if mxns/mouse
+	(progn
+	  (xterm-mouse-mode 1)
+	  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+	  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
+	  (global-set-key (kbd "<mouse-6>") (lambda () (interactive) (scroll-left -1)))
+	  (global-set-key (kbd "<mouse-7>") (lambda () (interactive) (scroll-left 1)))
+	  (setq mxns/mouse nil))
+      (progn
+	(xterm-mouse-mode nil)
+	(global-unset-key (kbd "<mouse-4>"))
+	(global-unset-key (kbd "<mouse-5>"))
+	(global-unset-key (kbd "<mouse-6>"))
+	(global-unset-key (kbd "<mouse-7>"))
+	(setq mxns/mouse 1)))))
+
+
+
+
