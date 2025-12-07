efcb8256eda4741221c92c2d9c7093716682ef41
Måns Rolandi <mxns@users.noreply.github.com>
2025-12-07 21:29:32 +0100

    neotree

--- init-project.el
+++ init-project.el
@@ -76,7 +76,8 @@ If no recent file is found, fallback to user selection via
 
 (use-package neotree
   :ensure t
-  :bind (("C-c t" . neotree-project-root-toggle))
+  :bind (("C-c t" . neotree-project-root-toggle)
+         ("C-c T" . neotree-project-collapse-others))
   :config
   (setq neo-show-hidden-files t)
   (setq neo-autorefresh t)
@@ -84,89 +85,50 @@ If no recent file is found, fallback to user selection via
   (setq neo-smart-open nil)
   (setq neo-window-width 30)
   
-  ;; Function to open neotree at project root
-  (defun neotree-project-root-toggle ())
-  
   (defun neotree-project-root ()
-  "Open neotree at the project root and find current file."
-  (interactive)
-  (let* ((project-root (if-let ((project (project-current)))
-                           (project-root project)
-                         default-directory))
-         (current-file (buffer-file-name)))
-    (save-selected-window
-      (if (neo-global--window-exists-p)
+    "Open neotree at the project root and find current file."
+    (interactive)
+    (let* ((project-root (if-let ((project (project-current)))
+                             (project-root project)
+                           default-directory))
+           (current-file (buffer-file-name)))
+      (save-selected-window
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-root)
+              (when current-file (neotree-find current-file)))
           (progn
+            (neotree-show)
             (neotree-dir project-root)
-            (when current-file (neotree-find current-file)))
-        (progn
-          (neotree-show)
-          (neotree-dir project-root)
-          (when current-file
-            (run-with-idle-timer 0.1 nil
-                                 (lambda ()
-                                   (save-selected-window
-                                     (neotree-find current-file))))))))))
-
+            (when current-file
+              (run-with-idle-timer 0.1 nil
+                                   (lambda ()
+                                     (save-selected-window
+                                       (neotree-find current-file))))))))))
+  
   (defun neotree-project-root-toggle ()
     "Toggle neotree at the project root and find current file."
     (interactive)
     (if (neo-global--window-exists-p)
         (neotree-hide)
       (neotree-project-root)))
+
+  (defun neotree-project-collapse-others ()
+    "Collapse all neotree nodes and show current file."
+    (interactive)
+    (when (neo-global--window-exists-p)
+      (save-selected-window
+        (neo-global--select-window)
+        (neotree-collapse-all)))
+    (neotree-project-root))
   
   (defun neotree-project-root-after-switch (&rest _args)
     "Open neotree at project root after switching projects."
-    (message "neotree-project-root-after-switch call")
     (when (and (project-current)
                (not current-prefix-arg))  ; skip if called with C-u
       (neotree-project-root)))
   
-  (advice-add 'mxns/project-switch-project :after #'neotree-project-root-after-switch)
-  )
-
-
-;; (use-package treemacs
-;;   :functions
-;;   mxns/treemacs-switch-project
-;;   mxns/project-switch-project
-;;   treemacs-do-remove-project-from-workspace
-
-;;   :init
-;;   (setq project-kill-buffers-display-buffer-list t)
-
-;;   :custom
-;;   (treemacs-git-mode -1)
-  
-;;   :bind
-;;   ("C-c s" . mxns/treemacs-toggle-lsp-symbols)
-;;   ("C-c t" . mxns/treemacs-toggle-preserve-window)
-
-;;   :config
-;;   (defun mxns/treemacs-toggle-lsp-symbols ()
-;;     "Toggle LSP Symbols window without changing focus."
-;;     (interactive)
-;;     (let ((current-window (selected-window)))
-;;       (if (get-buffer-window "*Lsp Symbols List*")
-;;           ;; If symbols window is visible, close it
-;;           (when-let ((window (get-buffer-window "*Lsp Symbols List*")))
-;;             (delete-window window))
-;;         ;; If symbols window is not visible, open it and return focus
-;;         (progn
-;;           (lsp-treemacs-symbols)
-;;           (select-window current-window)))))
-
-;;   (defun mxns/treemacs-toggle-preserve-window ()
-;;     "Toggle Treemacs without changing the selected window."
-;;     (interactive)
-;;     (save-selected-window
-;;       (treemacs)))
-
-;;   (defun mxns/treemacs-switch-project (&rest _)
-;;     "Function to run when switching projects."
-;;     (save-selected-window (treemacs-add-and-display-current-project)))
-
-;;   (advice-add 'mxns/project-switch-project :after #'mxns/treemacs-switch-project))
+  (advice-add 'mxns/project-switch-project :after #'neotree-project-root-after-switch))
 
 
 
@@ -204,5 +166,17 @@ If no recent file is found, fallback to user selection via
     map)
   "Keymap for project commands.")
 
+(which-key-add-keymap-based-replacements mxns/project-prefix-map
+    "f" "Find file (fd)"
+    "g" "Grep (rg)"
+    "r" "Query replace regexp"
+    "b" "Switch to buffer"
+    "C-b" "List buffers"
+    "p" "Open project"
+    "a" "Switch project"
+    "q" "Kill project"
+    "d" "Find directory"
+    "D" "Open in Dired"
+    "v" "VC directory")
 
 ;;; init-project.el ends here
